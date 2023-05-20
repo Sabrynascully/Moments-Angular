93edb79c85c4a01de2336845d5c2ca39de067664
Sabryna Marques <scullysabrina8@gmail.com>
2023-05-20 12:29:29 -0300

    Página de formulário

--- src/app/pages/new-moment/new-moment.component.ts
+++ src/app/pages/new-moment/new-moment.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-new-moment',
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./new-moment.component.css']
 })
 export class NewMomentComponent {
+
+btnText = 'Compartilhar!';
+
+constructor(){
+
+}
+
+ngOnInit(){
+
+}
 
 }
