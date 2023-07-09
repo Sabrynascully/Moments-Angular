01340df7caf9844c717f261d2b62dcdbd9e573f9
Sabryna Marques <scullysabrina8@gmail.com>
2023-07-09 17:28:26 -0300

    conclusão do projeto

--- src/app/pages/new-moment/new-moment.component.ts
+++ src/app/pages/new-moment/new-moment.component.ts
@@ -1,39 +1,52 @@
-import { Component, Input } from '@angular/core';
-import { Moment } from 'src/app/Moments';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
 import { MomentService } from 'src/app/services/moment.service';
-import {  MessagesService } from 'src/app/services/messages.service';
+
 import { Router } from '@angular/router';
+
+import { MessagesService } from 'src/app/services/messages.service';
+import { Moment } from 'src/app/Moments';
 
 @Component({
   selector: 'app-new-moment',
   templateUrl: './new-moment.component.html',
-  styleUrls: ['./new-moment.component.css']
+  styleUrls: ['./new-moment.component.css'],
 })
-export class NewMomentComponent {
+export class NewMomentComponent implements OnInit {
+  btnText: string = 'Compartilhar!';
+  image?: File;
 
-btnText = 'Compartilhar!';
+  constructor(
+    private momentService: MomentService,
+    private router: Router,
+    private messagesService: MessagesService
+  ) {}
 
-constructor(private momentService: MomentService, private messagesService: MessagesService, private router: Router){
+  ngOnInit(): void {}
 
-}
+  onFileSelected(event: any) {
+    const file: File = event.target.files[0];
 
-ngOnInit(){
+    this.image = file;
+  }
 
-}
+  buildForm() {}
 
-async createHandler(moment:Moment){
-const formData = new FormData()
-formData.append("title", moment.title)
-formData.append("description", moment.description)
-if(moment.image){
-  formData.append("image", moment.image)
-}
+  async createHandler(moment: Moment) {
+    const formData = new FormData();
 
-await this.momentService.createMoment(formData).subscribe();
+    formData.append('title', moment.title);
+    formData.append('description', moment.description);
 
-this.messagesService.add('Momento adicionado com sucesso!');
-this.router.navigate(['/'])
+    if (moment.image) {
+      formData.append('image', moment.image);
+    }
 
-}
+    await this.momentService.createMoment(formData).subscribe();
 
+    this.messagesService.add(`Momento adicionado com sucesso!`);
+
+    this.router.navigate(['/']);
+  }
 }
